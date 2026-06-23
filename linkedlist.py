0f53fef97658e0beb54cbda37cfb35773bd98190
Alekhya A <alekhyavilasagarapu12@gmail.com>
2026-06-23 09:38:05 +0530

    Updated code

--- linkedlist.py
+++ linkedlist.py
@@ -21,6 +21,77 @@ class LinkedList:
             return
         obj.next=self.head
         self.head = obj
+    def middle(self):
+        slow = self.head
+        fast = self.head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow.data
+    def remove_beg(self,data):
+        if self.head is None:
+            return
+        self.head=self.head.next
+    def remove_end(self,data):
+        itr = self.head
+        while itr.next.next:
+            itr = itr.next
+        itr.next = None
+    def add_pos(self, pos, data):
+        if pos == 0:
+            self.add_beg(data)
+            return
+        itr = self.head
+        for i in range(pos - 1):
+            itr = itr.next
+        new_node = Node(data)
+        new_node.next = itr.next
+        itr.next = new_node
+    def reverse(self):
+        prev = None
+        curr = self.head
+        while curr:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+        self.head = prev
+    def duplicate(self):
+        if self.head is None:
+            return
+        seen = set()
+        itr = self.head
+        prev = None
+        while itr:
+            if itr.data in seen:
+                prev.next = itr.next
+            else:
+                seen.add(itr.data)
+                prev = itr
+            itr = itr.next
+        return self.head
+    def remove_any_two(self, data):
+        if self.head is None:
+            return
+        count = 0
+        itr = self.head
+        prev = None
+        while itr:
+            if itr.data == data:
+                count += 1
+                if count <= 2:
+                    if prev is None:
+                        self.head = itr.next
+                    else:
+                        prev.next = itr.next
+            else:
+                prev = itr
+            itr = itr.next
+    
+        
+        
+    
+
         
         
     def display(self):
@@ -36,8 +107,15 @@ ll.add_end(80)
 ll.add_end(90)
 ll.add_beg(40)
 ll.display()
+ll.middle()
+ll.remove_beg(40)
+ll.remove_end(90)
+ll.display()
+l1=LinkedList()
+l1.duplicate()
+l1.remove_any_two(60)
+l1.display()
 
 
 
 
-   
